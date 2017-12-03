f07896573a090d3ca5dbf1304e9da6a9297563bb
Oleg Legun <oleg.legun@gmail.com>
2017-12-03 16:23:49 +0300

    implement drop target for dnd

--- src/components/events/SelectedEventCard.js
+++ src/components/events/SelectedEventCard.js
@@ -1,9 +1,14 @@
 /* @flow */
 import * as React from 'react'
+import { DropTarget } from 'react-dnd'
+import type { ConnectDropTarget } from 'react-dnd'
 import type { EventRecord } from '../../ducks/events'
 
 type Props = {
     event: EventRecord,
+    connectDropTarget: ConnectDropTarget,
+    canDrop: boolean,
+    hovered: boolean,
 }
 
 type State = {}
@@ -14,13 +19,18 @@ class SelectedEventCard extends React.Component<Props, State> {
     state = {}
 
     render() {
-        const { event } = this.props
-        return (
+        const { event, connectDropTarget, canDrop, hovered } = this.props
+
+        const backgroundColor = canDrop
+            ? hovered ? '#77ff77' : '#aaffaa'
+            : 'white'
+        return connectDropTarget(
             <div
                 style={{
                     width: 250,
                     height: 60,
-                    border: '1px solid black',
+                    border: `1px solid black`,
+                    backgroundColor: backgroundColor,
                     display: 'inline-block',
                     margin: 5,
                     padding: 10,
@@ -34,4 +44,11 @@ class SelectedEventCard extends React.Component<Props, State> {
     }
 }
 
-export default SelectedEventCard
+const spec = {}
+const collect = (connect, monitor) => ({
+    connectDropTarget: connect.dropTarget(),
+    canDrop: monitor.canDrop(),
+    hovered: monitor.isOver(),
+})
+
+export default DropTarget(['person'], spec, collect)(SelectedEventCard)
